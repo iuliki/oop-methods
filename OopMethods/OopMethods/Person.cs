3c96641732aa9e2c149e10045789abb685343d95
iuliki <iulia.bulugu@emanuel.ro>
2021-12-17 10:21:18 +0200

    first

---
+++ OopMethods/OopMethods/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopMethods
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return ($"{FirstName} {LastName}");
+            }
+        }
+        public void SayHello(string introductionFormula)//nu trebuie neaparat return 
+        {
+            Console.Write($"{introductionFormula}{FullName}" );
+              }
+
+         public int GetAge() //trebuie sa am neaparat return 
+        {
+            return DateTime.Today.Year - DateOfBirth.Year;
+        }
+     }
+}
+
+
+    
+   //instanta si static???
+   //static ar fi daca toata masinile din luma ar avea acelasi mototr, acelasi obict motor
+   //
---
+++ OopMethods/OopMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OopMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.FisrtName = "john";
+            p.LastName = "doe";
+            p.SayHello("Heloo, my name is ");
+
+        }
+    }
+}
